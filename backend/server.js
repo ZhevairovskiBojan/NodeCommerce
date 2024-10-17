dbeb42e23c550ad9217d8cdb95dc935613cd0c35
ZhevairovskiBojan <bojanzevairovskijs@gmail.com>
2024-10-17 20:21:33 +0200

    MongoDB Atlas is connected

--- backend/server.js
+++ backend/server.js
@@ -18,18 +18,17 @@ app.get('/', (req, res) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;  // Default to port 3000
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
-// Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI, {
-    // useNewUrlParser: true,
-    // useUnifiedTopology: true
-  })
-    .then(() => console.log('MongoDB connected successfully'))
-    .catch((err) => {
-      console.error('Error connecting to MongoDB:', err.message);
-      process.exit(1); // Exit if there's a MongoDB connection error
-    });
+  // useNewUrlParser: true,  // This ensures correct URL parsing.
+  // useUnifiedTopology: true  // This ensures proper behavior with MongoDB's new connection engine.
+})
+  .then(() => console.log('MongoDB connected successfully'))
+  .catch((err) => {
+    console.error('Error connecting to MongoDB:', err.message);
+    process.exit(1); // Exit if there's a MongoDB connection error
+  });
