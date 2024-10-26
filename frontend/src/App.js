35f359dc070c75cfcb20f35025806192bf6d6727
ZhevairovskiBojan <bojanzevairovskijs@gmail.com>
2024-10-26 18:26:50 +0200

    app js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,7 +14,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/cart" component={Cart} />
+        <Route path="/cart" element={<Cart />} />
        
         <Route
           path="/dashboard"
